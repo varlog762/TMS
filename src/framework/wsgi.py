7b5f59fdea3b7e8407cdecf8143af798627056ed
Gregory Pereverzev <boot762@gmail.com>
2020-10-13 21:19:40 +0300

    Научились читать хтмл из файла.

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -3,23 +3,16 @@ def application(environ, start_response):
     headers = {
         "Content-type": "text/html",
     }
-    payload = b"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Z37 Hello World!</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1>Z37 study project</h1>
-        <h2>123456</h2>
-        <h3>123456</h3>
-        <hr>
-        <p>This is a study project.</p>
-        <p>This is a first project.</p>
-        </body>
-        </html>
-    """
+    # payload = read_from_ind_html()
+
+    # --------------------------------------------
+    path = "src/static/index.html"
+    fp = open(path, "r")
+    payload = fp.read()
+    fp.close()
+
+    payload = payload.encode()
+    # --------------------------------------------
 
 
     start_response(status, list(headers.items()))
