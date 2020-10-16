6059bd518b6579555cf48460318553acc55ff8ef
Gregory Pereverzev <boot762@gmail.com>
2020-10-16 20:14:04 +0300

    add css-file add if/else to wsgi

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -2,15 +2,36 @@ from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
-    status = "200 OK"
-    headers = {
-        "Content-type": "text/html",
-    }
-    payload = read_from_ind_html()
+    url = environ['PATH_INFO']
+    if url == "/styles.css":
+        status = "200 OK"
+        headers = {
+            "Content-type": "text/css",
+        }
+        payload = read_from_styles_css()
+        start_response(status, list(headers.items()))
 
-    start_response(status, list(headers.items()))
+        yield payload
+    elif url == "/images/favicon.ico":
+        status = "200 OK"
+        headers = {
+            "Content-type": "img/png",
+        }
+        payload = read_from_favicon()
+        start_response(status, list(headers.items()))
 
-    yield payload
+        yield payload
+
+    else:
+        status = "200 OK"
+        headers = {
+            "Content-type": "text/html",
+        }
+        payload = read_from_ind_html()
+
+        start_response(status, list(headers.items()))
+
+        yield payload
 
 
 def read_from_ind_html():
@@ -20,4 +41,22 @@ def read_from_ind_html():
 
     payload = payload.encode()
     # str.encode() - перекодирует строку в байтстроку
+    return payload
+
+
+def read_from_styles_css():
+    path = DIR_STATIC / "styles.css"
+    with path.open("r") as fp:
+        payload = fp.read()
+
+    payload = payload.encode()
+    return payload
+
+
+def read_from_favicon():
+    path = DIR_STATIC / "/images/favicon.ico"
+    with path.open("r") as fp:
+        payload = fp.read()
+
+    payload = payload.encode()
     return payload
