8c849fc42c673cb338c5958a80134f14251bfdff
Gregory Pereverzev <boot762@gmail.com>
2020-10-30 13:40:32 +0300

    Доработана обработка ошибки 404.

--- src/handlers/handle_404.py
+++ src/handlers/handle_404.py
@@ -1,13 +1,41 @@
 import random
+from html import escape
 
 from framework.types import ResponseT
+from framework.utils import format_env_var
+from framework.utils import http_first
+from framework.utils import read_static
 
 
 def handle_404(environ) -> ResponseT:
+    url = environ["PATH_INFO"]
+    pin = random.randint(1, 999999)
+
+    environ_pairs = "\n".join(
+        f"<div class=\"pair {'http' if env_var_name.startswith('HTTP') else ''}\">"
+        f"<p>{escape(str(env_var_name))}</p>"
+        f"<p>{format_env_var(env_var_name, env_var_value)}</p>"
+        f"</div>"
+        for env_var_name, env_var_value in sorted(environ.items(), key=http_first)
+    )
+
+    base_html = read_static("_base.html", str)
+
+    html_404 = f"""
+    <h1>OOPS!</h1>
+        <hr>
+        <h2>The path you've looking for does not exist on this server.</h2>
+        <p class="url"><span>{url}</span></p>
+        <p>Pin: <span class="pin">{pin:>06}</span></p>
+        <div class="environ-table">
+        {environ_pairs}
+        <div>
+    """
+
+    document = base_html.format(page=html_404)
+
+    payload = document.encode()
     status = "404 Not Found"
     headers = {"Content-type": "text/html"}
 
-    numb = random.randint(900, 999)
-    payload = f"Your number is: {numb}. Page  not found!"
-
-    return status, headers, payload.encode("utf-8")
+    return status, headers, payload
