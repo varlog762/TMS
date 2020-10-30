9871af7b5d2ca36b052586b8da58305d94bfd26a
Gregory Pereverzev <boot762@gmail.com>
2020-10-30 14:06:53 +0300

    Xxx (#9)
    * новая ветка
    
    * добавил favicon.png из интернета
    
    * изменение индекс.хтмл
    
    * изменение индекс.хтмл
    
    * wfgegeege
    
    * wfgegeege
    
    * wfgegeege
    
    * wfgegeege
    
    * add picture
    
    * add png
    
    * 111
    
    * 111
    
    * чиню тесты
    
    * mimetypes adding
    
    * Определяем Content-type для HTTP-ответа с помощью библиотеки mimetypes
    
    * Поправили код
    
    * add error 404 exception
    
    * Добавил кнопочку в Index.html
    
    * Создан пакет handlers, в нем созданы хендлеры для обработки и вывода страниц. HTML документы разделены на содержимое и общую для для всех документов (_base.html) части.
    
    * Изменены импорты. Функция read_static вынесена в модуль utils.py. Изменена картинка index.html
    
    * Добавлен хендлер handle_picture
    
    * Добавлен хендлер handle_picture
    
    * Добавлен хендлер handle_picture
    
    * Сделан make format, исправлены ошибки предыдущего мержа.
    
    * КосметитечÑеский ремонт index.html
    
    * Доработана обработка ошибки 404.
    
    * Сделан make format, исправлены ошибки предыдущего мержа.

--- src/framework/utils.py
+++ src/framework/utils.py
@@ -1,9 +1,47 @@
+import re
+from html import escape
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import Tuple
+
 from framework.consts import DIR_STATIC
 
 
-def read_static(file_name) -> bytes:
+def http_first(value: Tuple[str, Any]) -> tuple:
+    if value[0].startswith("HTTP"):
+        return 0, value
+    return 1, value
+
+
+def format_env_var(name: str, value: str) -> str:
+    formatter = get_formatter(name)
+    new = str(value)
+    new = formatter(new)
+    new = escape(new)
+    new = re.sub("\n", "<br>", new)
+
+    return new
+
+
+def get_formatter(env_var_name: str) -> Callable[[str], str]:
+    if env_var_name.endswith("PATH"):
+        return lambda _value: "\n".join(_value.split(":"))
+    if "ACCEPT" in env_var_name:
+        return lambda _v: "\n".join(re.split(r"[\s,]+", _v))
+    return lambda _v: _v
+
+
+def read_static(file_name: str, converter: Callable = bytes) -> Any:
     path = DIR_STATIC / file_name
-    with path.open("rb") as fp:
+
+    modes: Dict[Any, str] = {
+        str: "r",
+    }
+
+    mode = modes.get(converter, "rb")
+
+    with path.open(mode) as fp:
         payload = fp.read()
 
-    return payload
+    return converter(payload)
